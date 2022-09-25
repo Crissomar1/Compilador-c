78134c0de10a1b72c8547aca5a32370480137df2
Crissomar1 <cristian.morales@alumno.udg.mx>
2022-09-25 00:50:18 -0500

    gramatica 0.9

---
+++ gramatica.h
@@ -0,0 +1,59 @@
+#ifndef _GRAMATICA
+#define _GRAMATICA
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class Gramatica
+{
+    public:
+    int reglas;
+    vector<int> idRegla;
+    vector<int> lonRegla;
+    vector<string> simRegla;
+    int filas, columnas;
+    vector<vector<int> > tablaLR;
+    Gramatica(/* args */);
+    ~Gramatica();
+};
+
+Gramatica::Gramatica(/* args */)
+{
+    //leer archivo
+    ifstream archivo("GramaticaCompilador/compilador.lr", ios::in);
+    if(archivo.fail()){
+        cout << "No se pudo abrir el archivo" << endl;
+        exit(1);
+    }
+    //leer reglas
+    archivo >> reglas;
+    idRegla.resize(reglas);
+    lonRegla.resize(reglas);
+    simRegla.resize(reglas);
+    for(int i = 0; i < reglas; i++){
+        archivo >> idRegla[i];
+        archivo >> lonRegla[i];
+        archivo >> simRegla[i];
+    }
+    //leer tabla
+    archivo >> filas >> columnas;
+    tablaLR.resize(filas);
+    for(int i = 0; i < filas; i++){
+        tablaLR[i].resize(columnas);
+        for(int j = 0; j < columnas; j++){
+            archivo >> tablaLR[i][j];
+        }
+    }
+    archivo.close();
+    
+}
+
+Gramatica::~Gramatica()
+{
+}
+
+
+#endif
--- principal.cpp
+++ principal.cpp
@@ -6,33 +6,35 @@
 #include "lexico.cpp"
 #include "pila.cpp"
 #include "nodoSintactico.h"
+#include "gramatica.h"
 
 using namespace std;
 
 
 int main(int argc, char *argv[]){
 
-    int tablaLR[5][4]={
-        2, 0, 0,1,
-        0, 0, -1,0,
-        0, 3, -3,0,
-        2,0,0,4,
-        0,0,-2,0
-    };
-    int idReglas[2]={2,2};
-    int lonReglas[2]={3,1};
+    // int tablaLR[5][4]={
+    //     2, 0, 0,1,
+    //     0, 0, -1,0,
+    //     0, 3, -3,0,
+    //     2,0,0,4,
+    //     0,0,-2,0
+    // };
+    // int idReglas[2]={2,2};
+    // int lonReglas[2]={3,1};
 
+    Gramatica gramatica;
 
     Pila pila;
     int fila, columna, accion;
     bool aceptacion;
-    nodoSintactico *nodo= new nodoSintactico('E',2,0,"$");
+    nodoSintactico *nodo= new nodoSintactico('E',TipoSimbolo::PESOS,0,"$");
 
     pila.push(/*TipoSimbolo::PESOS*/nodo);//2 en el ejemplo y ejercicios actual
 
 
     Lexico lexico; 
-    string entrada = "a+b+c+d+e+f";
+    string entrada = "int main(){int a; a=1;}";
     cout << "Entrada: " << entrada << endl;
     
     lexico.entrada(entrada);
@@ -41,18 +43,19 @@ int main(int argc, char *argv[]){
         lexico.sigSimbolo();
 
         fila=pila.top()->transicion;
-        switch(lexico.tipo){    //Un pequeño traductor de analizador lexico para que funcione con nuestra tabla de transiciones
-            case TipoSimbolo::IDENTIFICADOR:
-                columna=0;
-                break;
-            case TipoSimbolo::OPADIC:
-                columna=1;
-                break;
-            case TipoSimbolo::PESOS:
-                columna=2;
-                break;
-        }
-        accion=tablaLR[fila][columna];
+        // switch(lexico.tipo){    //Un pequeño traductor de analizador lexico para que funcione con nuestra tabla de transiciones
+        //     case TipoSimbolo::IDENTIFICADOR:
+        //         columna=0;
+        //         break;
+        //     case TipoSimbolo::OPADIC:
+        //         columna=1;
+        //         break;
+        //     case TipoSimbolo::PESOS:
+        //         columna=2;
+        //         break;
+        // }
+        columna=lexico.tipo;
+        accion=gramatica.tablaLR[fila][columna];
 
         pila.muestra();
         cout << "entrada: " << lexico.simbolo << endl;
@@ -63,11 +66,11 @@ int main(int argc, char *argv[]){
         }
         if (accion<=-2){
             int rule=abs(accion)-2;//se resta 1 por el offset a la representacion de reglas en negativo y se resta otro por dispocision de arreglos de reglas
-            int red=lonReglas[rule];
-            int ter=idReglas[rule];
+            int red=gramatica.lonRegla[rule];
+            int ter=gramatica.idRegla[rule];
             int i=0;
             //crear un nodo para la regla
-            nodoSintactico *nodo= new nodoSintactico('N',ter,0,"E");
+            nodoSintactico *nodo= new nodoSintactico('N',accion,ter,gramatica.simRegla[rule]);
             
             while(i<red){
                 nodo->hijos.push_front(pila.top());
@@ -76,18 +79,7 @@ int main(int argc, char *argv[]){
             }
             
             fila=pila.top()->transicion;
-            switch(lexico.tipo){    //Un pequeño traductor de analizador lexico para que funcione con nuestra tabla de transiciones
-            case TipoSimbolo::IDENTIFICADOR:
-                columna=0;
-                break;
-            case TipoSimbolo::OPADIC:
-                columna=1;
-                break;
-            case TipoSimbolo::PESOS:
-                columna=2;
-                break;
-             }
-            accion=tablaLR[fila][3];
+            accion=gramatica.tablaLR[fila][ter];
             nodo->transicion=accion;
             pila.push(nodo);
             pila.muestra();
